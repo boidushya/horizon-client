f743576c26791626b8168e77804357bea6775289
kg-kartik <goel.kartik39@gmail.com>
2022-05-29 15:44:29 +0530

    Fixed user type

--- types/Users.ts
+++ types/Users.ts
@@ -1,33 +1,9 @@
-import Clubs from "./Clubs";
-import EventReg from "./EventReg";
-
 interface Users {
 	_id?: string;
-	username?: string;
 	name: string;
-	type: string;
-	secretPassword: string;
-	titleInClubs?: Array<{ club: Clubs; title: string }>;
 	email: string;
-	studentId?: string;
-	eventsConducted?: Array<{
-		answerOfQuestions: { ques: string; answer: string };
-		event: string;
-	}>;
-	college: string;
-	clubs?: Array<Clubs>; //All clubs enrolled in
-	notificationOn?: Array<Clubs>; //All clubs whose notif is turned on
-	events?: Array<{
-		answerOfQuestions: { ques: string; answer: string };
-		event: string;
-	}>; //All clubs entolled in
 	password?: string;
-}
-
-export interface RegisteredUser {
-	_id: string;
-	answerOfQuestions: Array<{ ques: string; answer: string; _id: string }>;
-	user: { name: string; email: string; studentId: string };
+	phone:string;
 }
 
 export default Users;
